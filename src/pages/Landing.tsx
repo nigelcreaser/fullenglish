4138d4541b8f1bed4dabe181e4f98fbf613c1d41
nigelcreaser <33607249+nigelcreaser@users.noreply.github.com>
2026-02-06 19:48:47 +0000

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MapPin, Award, Check, Instagram, Twitter } from 'lucide-react';
-
+<!-- MailerLite Universal -->
+<script>
+    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1908410');
+</script>
+<!-- End MailerLite Universal -->
 function Landing() {
   const [email, setEmail] = useState('');
   const [submitted, setSubmitted] = useState(false);
